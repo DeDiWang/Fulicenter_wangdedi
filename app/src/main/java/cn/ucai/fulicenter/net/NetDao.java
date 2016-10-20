db3a1ef10b9ca18f641f4782f7fe4d1b058cfa1f
王德帝 <1103930991@qq.com>
2016-10-20 12:02:47 +0800

    添加Dao层中分类首页大类和小类数据下载的网络请求的方法

--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -3,6 +3,9 @@ package cn.ucai.fulicenter.net;
 import android.content.Context;
 
 import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.bean.BoutiqueBean;
+import cn.ucai.fulicenter.bean.CategoryChildBean;
+import cn.ucai.fulicenter.bean.CategoryGroupBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
@@ -12,15 +15,15 @@ import cn.ucai.fulicenter.utils.OkHttpUtils;
  */
 public class NetDao {
     /**
-     * 下载新品首页数据
+     * 下载新品首页数据或精品二级页面数据
      * @param context
      * @param pageId
      * @param listener
      */
-    public static void downloadGoodsList(Context context, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
+    public static void downloadGoodsList(Context context,int catId, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
     OkHttpUtils<NewGoodsBean[]> utils=new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
-                .addParam(I.NewAndBoutiqueGoods.CAT_ID,I.CAT_ID+"")
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID,catId+"")
                 .addParam(I.PAGE_ID,pageId+"")
                 .addParam(I.PAGE_SIZE,I.PAGE_SIZE_DEFAULT+"")
                 .targetClass(NewGoodsBean[].class)
@@ -33,11 +36,53 @@ public class NetDao {
      * @param goodsId
      * @param listener
      */
-    public static void downloadGoodDetails(Context context,int goodsId,OkHttpUtils.OnCompleteListener<GoodsDetailsBean> listener){
+    public static void downloadGoodDetails(Context context, int goodsId,
+                  OkHttpUtils.OnCompleteListener<GoodsDetailsBean> listener){
         OkHttpUtils<GoodsDetailsBean> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
                 .addParam(I.GoodsDetails.KEY_GOODS_ID,goodsId+"")
                 .targetClass(GoodsDetailsBean.class)
+                .execute(listener);
+    }
+
+    /**
+     * 下载精品首页商品数据
+     * @param context
+     * @param listener
+     */
+    public static void downloadBoutique(Context context,
+                 OkHttpUtils.OnCompleteListener<BoutiqueBean[]> listener){
+        OkHttpUtils<BoutiqueBean[]> utils=new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_BOUTIQUES)
+                .targetClass(BoutiqueBean[].class)
+                .execute(listener);
+    }
+
+    /**
+     * 下载分类首页Group数据
+     * @param context
+     * @param listener
+     */
+    public static void downloadCategoryGroupList(Context context,
+                 OkHttpUtils.OnCompleteListener<CategoryGroupBean[]> listener){
+        OkHttpUtils<CategoryGroupBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_GROUP)
+                .targetClass(CategoryGroupBean[].class)
+                .execute(listener);
+    }
+    /**
+     * 下载分类首页Child数据
+     * @param context
+     * @param parentId
+     * @param listener
+     */
+    public static void downloadCategoryChildList(Context context,
+                 int parentId,
+                 OkHttpUtils.OnCompleteListener<CategoryChildBean[]> listener){
+        OkHttpUtils<CategoryChildBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_CHILDREN)
+                .addParam(I.CategoryChild.PARENT_ID,parentId+"")
+                .targetClass(CategoryChildBean[].class)
                 .execute(listener);
     }
 }
