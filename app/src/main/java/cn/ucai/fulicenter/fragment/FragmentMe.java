579c390efe99323b3658c83316213185a149afca
王德帝 <1103930991@qq.com>
2016-10-24 20:47:43 +0800

    在个人中心的Fragment中判断是否已经登录过，若是则设置头像和昵称，若没有则跳到登录界面

--- app/src/main/java/cn/ucai/fulicenter/fragment/FragmentMe.java
+++ app/src/main/java/cn/ucai/fulicenter/fragment/FragmentMe.java
@@ -1,29 +1,57 @@
 package cn.ucai.fulicenter.fragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.Dao.SharedPreferencesUtils;
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.UserAvatar;
+import cn.ucai.fulicenter.utils.ImageLoader;
+import cn.ucai.fulicenter.utils.L;
+import cn.ucai.fulicenter.utils.MFGT;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentMe extends Fragment {
-
+    private static final String TAG = FragmentMe.class.getSimpleName();
+    @BindView(R.id.ivUserThumb)
+    ImageView ivUserThumb;
+    @BindView(R.id.tvNick)
+    TextView tvNick;
 
     public FragmentMe() {
         // Required empty public constructor
     }
-
-
+    Context mContext;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        View view = inflater.inflate(R.layout.fragment_me, container, false);
+        ButterKnife.bind(this, view);
+        mContext = getActivity();
+        initData();
+        return view;
     }
 
+    private void initData() {
+        UserAvatar user = FuLiCenterApplication.getUser();
+        L.e(TAG+" user=="+user);
+        if(user!=null){
+            tvNick.setText(user.getMuserNick());
+            ImageLoader.setAvatar(user,mContext,ivUserThumb);
+        }else{
+            MFGT.gotoLoginActivity((Activity) mContext);
+        }
+    }
 }
