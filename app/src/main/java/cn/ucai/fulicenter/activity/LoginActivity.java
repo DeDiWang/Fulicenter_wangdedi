88455ca9ad901125c0914cd9cb40b8f29fd6bfd5
王德帝 <1103930991@qq.com>
2016-10-24 20:42:18 +0800

    登陆成功后将用户名保存在首选项中，将用户信息保存在内存和数据库中

--- app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
@@ -1,6 +1,6 @@
 package cn.ucai.fulicenter.activity;
 
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,20 +9,23 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.Dao.SharedPreferencesUtils;
+import cn.ucai.fulicenter.Dao.UserDao;
 import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.UserAvatar;
 import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
+import cn.ucai.fulicenter.utils.ResultUtils;
 
 public class LoginActivity extends AppCompatActivity {
     private final String TAG = LoginActivity.class.getSimpleName();
@@ -36,11 +39,13 @@ public class LoginActivity extends AppCompatActivity {
     EditText etPassword;
     String userName;
     String password;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
         ButterKnife.bind(this);
+        mContext=this;
         initView();
 
     }
@@ -55,8 +60,9 @@ public class LoginActivity extends AppCompatActivity {
         password = etPassword.getText().toString();
         switch (view.getId()) {
             case R.id.btnLogin:
-                checkInput();
-                login();
+                if(checkInput()){
+                    login();
+                }
                 break;
             case R.id.btnFreeRegister:
                 startActivityForResult(new Intent(this,RegisterActivity.class), I.REQUEST_CODE_REGISTER);
@@ -65,56 +71,75 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
-        NetDao.login(this, userName, password, new OkHttpUtils.OnCompleteListener<Result>() {
+        NetDao.login(this, userName, password, new OkHttpUtils.OnCompleteListener<String>() {
             @Override
-            public void onSuccess(Result result) {
-                if(result==null){
-                    Toast.makeText(LoginActivity.this, "登录失败！", Toast.LENGTH_SHORT).show();
+            public void onSuccess(String str) {
+                Result result = ResultUtils.getResultFromJson(str, UserAvatar.class);
+                if(result.toString()==null){
+                    CommonUtils.showShortToast(getResources().getString(R.string.login_fail));
                 }else{
                     if(result.isRetMsg()){
-                        Toast.makeText(LoginActivity.this, "登录成功!", Toast.LENGTH_SHORT).show();
-                        //拿到用户信息
-                        //UserAvatar user = (UserAvatar) result.getRetData();
-                        //L.e(TAG,user.toString());
-                        //MFGT.finish(LoginActivity.this);
+                        CommonUtils.showLongToast(getResources().getString(R.string.login_success));
+                        //得到用户信息
+                        UserAvatar user = (UserAvatar) result.getRetData();
+                        L.e("用户信息："+user.toString());
+                        //将用户信息保存在数据库中
+                        UserDao userDao = new UserDao(LoginActivity.this);
+                        boolean isSuccess = userDao.saveUser(user);
+                        if(isSuccess){
+                            //将登陆成功后的用户名保存在首选项中
+                            SharedPreferencesUtils.getInstance(mContext).saveUser(user.getMuserName());
+                            //将登陆成功后的用户信息保存在内存中
+                            FuLiCenterApplication.setUser(user);
+                            //跳转回MainActivity
+                            setResult(RESULT_OK);
+                            MFGT.finish(LoginActivity.this);
+                        }else {
+                            CommonUtils.showShortToast(getResources().getString(R.string.user_database_error));
+                        }
                     }else {
                         if(result.getRetCode()==I.MSG_LOGIN_ERROR_PASSWORD){
-                            Toast.makeText(LoginActivity.this, "密码错误!", Toast.LENGTH_SHORT).show();
+                            CommonUtils.showShortToast(getResources().getString(R.string.error_password));
                         }else if(result.getRetCode()==I.MSG_LOGIN_UNKNOW_USER){
-                            Toast.makeText(LoginActivity.this, "该用户不存在!", Toast.LENGTH_SHORT).show();
+                            CommonUtils.showShortToast(getResources().getString(R.string.unknow_user));
                         }else {
-                            Toast.makeText(LoginActivity.this, "登录失败！", Toast.LENGTH_SHORT).show();
+                            CommonUtils.showShortToast(getResources().getString(R.string.login_fail));
                         }
                     }
                 }
             }
             @Override
             public void onError(String error) {
-                Toast.makeText(LoginActivity.this, "登录失败！", Toast.LENGTH_SHORT).show();
+                CommonUtils.showShortToast(getResources().getString(R.string.login_fail));
                 L.e(error);
             }
         });
     }
 
-    private void checkInput() {
+    private boolean checkInput() {
         if(TextUtils.isEmpty(userName)){
-            etUserName.setError("用户名不能为空");
+            etUserName.setError(getResources().getString(R.string.user_name_connot_be_empty));
             etUserName.requestFocus();
-            return;
+            return false;
         }
         if(TextUtils.isEmpty(password)){
-            etPassword.setError("密码不能为空");
+            etPassword.setError(getResources().getString(R.string.password_connot_be_empty));
             etPassword.requestFocus();
-            return;
+            return false;
         }
+        return true;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==I.REQUEST_CODE_REGISTER && resultCode==RESULT_OK){
-            String userName = data.getStringExtra("userName");
+            String userName = data.getStringExtra(I.User.USER_NAME);
             etUserName.setText(userName);
+            if(!etPassword.getText().toString().isEmpty()){
+                etPassword.setText(null);
+            }
+            etPassword.requestFocus();
         }
     }
 
