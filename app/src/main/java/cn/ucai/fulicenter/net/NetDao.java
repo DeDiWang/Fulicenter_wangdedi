cd776f83f23256726e08ceed42774801c58a4d27
王德帝 <1103930991@qq.com>
2016-10-25 21:06:04 +0800

    在NetDao中添加修改昵称和更新头像的方法

--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -1,6 +1,9 @@
 package cn.ucai.fulicenter.net;
 
+import android.app.Activity;
 import android.content.Context;
+
+import java.io.File;
 
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.bean.BoutiqueBean;
@@ -8,18 +11,12 @@ import cn.ucai.fulicenter.bean.CategoryChildBean;
 import cn.ucai.fulicenter.bean.CategoryGroupBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.utils.MD5;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
 
-/**
- * Created by 11039 on 2016/10/17.
- */
 public class NetDao {
-    /**
-     * 下载新品首页数据或精品二级页面数据
-     * @param context
-     * @param pageId
-     * @param listener
-     */
+    //下载新品首页数据或精品二级页面数据
     public static void downloadGoodsList(Context context,int catId, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
     OkHttpUtils<NewGoodsBean[]> utils=new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
@@ -30,12 +27,8 @@ public class NetDao {
                 .execute(listener);
     }
 
-    /**
-     * 下载指定商品详情数据
-     * @param context
-     * @param goodsId
-     * @param listener
-     */
+
+    //下载指定商品详情数据
     public static void downloadGoodDetails(Context context, int goodsId,
                   OkHttpUtils.OnCompleteListener<GoodsDetailsBean> listener){
         OkHttpUtils<GoodsDetailsBean> utils = new OkHttpUtils<>(context);
@@ -45,11 +38,7 @@ public class NetDao {
                 .execute(listener);
     }
 
-    /**
-     * 下载精品首页商品数据
-     * @param context
-     * @param listener
-     */
+    //下载精品首页商品数据
     public static void downloadBoutique(Context context,
                  OkHttpUtils.OnCompleteListener<BoutiqueBean[]> listener){
         OkHttpUtils<BoutiqueBean[]> utils=new OkHttpUtils<>(context);
@@ -58,11 +47,7 @@ public class NetDao {
                 .execute(listener);
     }
 
-    /**
-     * 下载分类首页Group数据
-     * @param context
-     * @param listener
-     */
+    //下载分类首页Group数据
     public static void downloadCategoryGroupList(Context context,
                  OkHttpUtils.OnCompleteListener<CategoryGroupBean[]> listener){
         OkHttpUtils<CategoryGroupBean[]> utils = new OkHttpUtils<>(context);
@@ -70,12 +55,8 @@ public class NetDao {
                 .targetClass(CategoryGroupBean[].class)
                 .execute(listener);
     }
-    /**
-     * 下载分类首页Child数据
-     * @param context
-     * @param parentId
-     * @param listener
-     */
+
+    //下载分类首页Child数据
     public static void downloadCategoryChildList(Context context,
                  int parentId,
                  OkHttpUtils.OnCompleteListener<CategoryChildBean[]> listener){
@@ -83,6 +64,68 @@ public class NetDao {
         utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_CHILDREN)
                 .addParam(I.CategoryChild.PARENT_ID,parentId+"")
                 .targetClass(CategoryChildBean[].class)
+                .execute(listener);
+    }
+
+    //http://101.251.196.90:8000/FuLiCenterServerV2.0/findGoodsDetails?cat_id=345&page_id=1&page_size=10
+    //下载分类二级页面一组商品数据
+    public static void downloadCategory2ChildList(Context context,int catId,int pageId,
+                                                  OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener){
+        OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOODS_DETAILS)
+                .addParam(I.Category.KEY_CAT_ID,String.valueOf(catId))
+                .addParam(I.PAGE_ID,String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE_DEFAULT))
+                .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+
+    //用户注册
+    public static void register(Context context, String userName,
+                                String nick, String password,
+                                OkHttpUtils.OnCompleteListener<Result> listener){
+        OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.NICK,nick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .targetClass(Result.class)
+                .post()
+                .execute(listener);
+    }
+
+    //用户登录
+    public static void login(Context context,String userName,String password,
+                             OkHttpUtils.OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.PASSWORD,MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    //更新用户昵称
+    public static void updateNick(Activity context, String muserName,String newNick,OkHttpUtils.OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME,muserName)
+                .addParam(I.User.NICK,newNick)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    //更新头像的方法
+    ///storage/emulated/0/Android/data/cn.ucai.fulicenter/files/Pictures/
+    // storage/emulated/0/Android/data/cn.ucai.fulicenter/files/Pictures/user_avatar/wangdedi123.jpg
+    //http://101.251.196.90:8000/FuLiCenterServerV2.0/updateAvatar?name_or_hxid=a952702&avatarType=user_avatar
+    public static void updateAvatar(Activity context, String muserName, File file, OkHttpUtils.OnCompleteListener<String> listener){
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,muserName)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
                 .execute(listener);
     }
 }
