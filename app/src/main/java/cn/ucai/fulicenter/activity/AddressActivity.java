a451ca442f289d0e67d8ab4ac479f47b9ccc3d80
王德帝 <1103930991@qq.com>
2016-10-28 15:20:07 +0800

    在订单界面添加各种信息验证

--- app/src/main/java/cn/ucai/fulicenter/activity/AddressActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/AddressActivity.java
@@ -2,18 +2,22 @@ package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
 
@@ -39,7 +43,6 @@ public class AddressActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         String cartIds = getIntent().getStringExtra("cartIds");
         arrCartId = cartIds.substring(0,cartIds.length()).split(",");
-        L.e(TAG+" arrCartId=="+Arrays.toString(arrCartId));
         orderPrice = getIntent().getDoubleExtra("orderPrice", 0);
         initView();
     }
@@ -55,8 +58,32 @@ public class AddressActivity extends AppCompatActivity {
                 MFGT.finish(this);
                 break;
             case R.id.btnBuy:
-
+                String userName = etName.getText().toString();
+                if(TextUtils.isEmpty(userName)){
+                    etName.setError("用户名不能为空");
+                    etName.requestFocus();
+                    return;
+                }
+                String number = etNumber.getText().toString();
+                if(TextUtils.isEmpty(number)){
+                    etNumber.setText("手机号不能为空");
+                    etNumber.requestFocus();
+                    return;
+                }
+                if(!number.matches("[\\d]{11}")){
+                    etNumber.setError("手机号格式不正确");
+                    etNumber.requestFocus();
+                    return;
+                }
+                String address = etAddress.getText().toString();
+                if(TextUtils.isEmpty(address)){
+                    etAddress.setError("地址不能为空");
+                    etAddress.requestFocus();
+                    return;
+                }
+                CommonUtils.showLongToast(orderPrice+"元");
                 break;
         }
     }
+
 }
